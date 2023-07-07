2287ee7171974e1103c8dd1edf7f9a2aa692884e
Shang Yi Ku <kushangyi2001@gmail.com>
2023-07-07 21:07:57 +0800

    Add Firebase to relay authentication

---
+++ ServerProject-DONT-IMPORT-INTO-UNITY/LRM/FirebaseAuthenticator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using FirebaseAdmin.Auth;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using LightReflectiveMirror;
+
+namespace LRM
+{
+    public class FirebaseAuthenticator
+    {
+        private static bool _firebaseAppInitialized = false;
+
+        public FirebaseAuthenticator()
+        {
+            if (!_firebaseAppInitialized)
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.GetApplicationDefault()
+                });
+                _firebaseAppInitialized = true;
+
+                Program.WriteLogMessage("FirebaseApp initialized!");
+            }
+        }
+
+        public async Task<bool> AuthenticateClient(string token)
+        {
+            try
+            {
+                await VerifyTokenGetUid(token);
+                return true;
+            }
+            catch (FirebaseAuthException e)
+            {
+                switch (e.AuthErrorCode)
+                {
+                    case AuthErrorCode.ExpiredIdToken:
+                        Program.WriteLogMessage("Expired token!");
+                        break;
+                    case AuthErrorCode.InvalidIdToken:
+                    case AuthErrorCode.RevokedIdToken:
+                        Program.WriteLogMessage("Invalid token!");
+                        break;
+                    case AuthErrorCode.UserNotFound:
+                        Program.WriteLogMessage("User not found!");
+                        break;
+                    default:
+                        Program.WriteLogMessage("Unknown Firebase authentication error!");
+                        break;
+                }
+            }
+            catch (ArgumentException)
+            {
+                Program.WriteLogMessage("Invalid token!");
+            }
+
+            return false;
+        }
+
+        private async Task<string> VerifyTokenGetUid(string token)
+        {
+            FirebaseToken decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(token);
+            // Console.WriteLine(decodedToken.Uid);
+            return decodedToken.Uid;
+        }
+    }
+}
